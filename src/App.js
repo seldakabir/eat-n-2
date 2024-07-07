49d3eacd2bb89ac1eed371874d25a1fe887d8ac9
selda kabir <seldaakabirnejat@gmail.com>
2024-07-08 01:20:21 +0300

    newFriend

--- src/App.js
+++ src/App.js
@@ -63,11 +63,28 @@ function Button({ children,onChangeShowAddList }) {
 }
 
 function AddFriend() {
-  return <form className="form-add-friend">
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('https://i.pravatar.cc/48?u')
+  
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!name || !image) return;
+    const id=crypto.randomUUID()
+    const newFriend = {
+      id,
+      name,
+      image: `${image}?=${id}`,
+      balance:0
+      
+    }
+    setName('')
+    setImage('https://i.pravatar.cc/48?u')
+  }
+  return <form className="form-add-friend" onSubmit={handleSubmit}>
     <label>👫 Friend name</label>
-    <input type="text"></input>
+    <input type="text" value={name} onChange={e=>setName(e.target.value)}></input>
      <label>🌄 Image URL</label>
-    <input type="text"></input>
+    <input type="text" value={image} onchange={e=>setImage(e.target.value)}></input>
     <Button>Add</Button>
   </form>
 }
